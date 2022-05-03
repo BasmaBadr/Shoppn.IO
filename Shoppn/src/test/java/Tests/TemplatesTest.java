f80cd81935a4ed4f963aea804eac3fe8566bb0d8
Basma <basma.badr1010@gmail.com>
2022-05-03 15:11:28 +0200

    Changes

--- Shoppn/src/test/java/Tests/TemplatesTest.java
+++ Shoppn/src/test/java/Tests/TemplatesTest.java
@@ -6,6 +6,8 @@ import Pages.TemplatesPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -20,11 +22,14 @@ public class TemplatesTest extends TestBase {
         loginPage.loginWithValidUNandPW("jahiji7855@host1s.com","12345678");
         Thread.sleep(20000);
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", templatesPage.templatesPage);
-       // templatesPage.scrollDown();
-//        templatesPage.openTemplatesTab();
-        //Assert.assertTrue(templatesPage.templatesPage.isDisplayed());
-//        Assert.assertTrue(templatesPage.templatesText.getText().contains("القوالب"));
-//        System.out.println(templatesPage.templatesText.getText());
+
+        templatesPage.openTemplatesTab();
+
+        WebDriverWait wait = new WebDriverWait(driver ,20);
+        wait.until(ExpectedConditions.visibilityOf(templatesPage.templatesText));
+
+        Assert.assertTrue(templatesPage.templatesText.getText().contains("القوالب"));
+        System.out.println(templatesPage.templatesText.getText());
     }
     }
 
